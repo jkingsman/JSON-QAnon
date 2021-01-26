4d0c1fb77fa168492b28812209c4819da72c1b41
jkingsman <jack.kingsman@gmail.com>
2021-01-25 20:54:02 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# QAnon is a dangerous cult. This archive is for research purposes only, and I do _not_ endorse any material in this repo.
+
+# QAnon Post Dataset
+
+In `posts.json` is an archive of all QAnon posts as scraped from https://qposts.online as of Jan 25 2020. The JSON has been dumped with `ensure_ascii=False` so should be UTF-8 but there may be some encoding gotchas I haven't caught. I did my best in terms of avoiding capture glitches/bad logic but I didn't read through all 4k posts so caveat emptor; I make no guarantees of data integrity.
+
+Posts reference images which I have opted not to include in this repo due to their distasteful content; the text is already quite enough and then some. You should be able to download the images with the mirror script below if you so desire; the `file` in `images` refers to the filename of the image as referred to by https://qposts.online at the time of indexing. I cannot speak to the durability of these references but they will be accurate if you run the extraction yourself (i.e. if the file naming scheme is changed, this script will pick it up).
+
+## Do it Yourself
+
+Took me about two hours for a total mirror on a terrible hotel wifi using a one second pause between requests; yours will probably go much faster on good internet (but remember to be a good netizen and rate limit requests, especially to a non-API. Depending on how low of a profile you want to keep, bump up the `--wait=1` option higher to wait more than one second between each request).
+
+To run your own extraction, update `DIRECTORY` in `collate.py` to point at the HTML location after mirroring, enter a `venv` and install the `requirements.txt`, then let it rip. It will dump the results as a JSON array to `posts.json` (should take a few seconds).
+
+Clone this repo, then run:
+
+```bash
+# setup + installation
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+chmod +x collate.py
+
+# site mirroring
+wget --wait=1 --level=inf --recursive --page-requisites --user-agent=Mozilla --no-parent --convert-links --adjust-extension --no-clobber --restrict-file-names=windows -e robots=off https://qposts.online/
+
+# collation; results in posts.json
+# remember to update DIRECTORY if the posts aren't in ./qposts.online/page relative to the script
+./collate.py
+```
+
+## Schema
+
+Actual JSON schemas give me a headache, so here it is in markdown form. If someone is really desperate for a JSON schema, I can probably scrape something together. This schema assumes all keys are on all objects unless it is stated as optional.
+
+The JSON takes the form of an array of `post` objects. A post consists of:
+
+* `post_metadata`: an object containing misc. information about the post (object)
+  * `author`: the author of the post; usually `Q` or `Anonymous` (string)
+  * `source`: an object containing information about the post's origin
+    * `board`: the chan board the post came from (string)
+    * `site`: one of `4ch`, `8ch`, or `8kun`, indicating the site the post is from (4chan, 8chan, or 8kun) (string)
+    * `link`: link to the original post (optional) (string)
+  * `time`: epoch timestamp of posting time (integer/timestamp)
+* `text`: the text of the post with newlines delimited by literal `\n` (string, optional)
+* `images`: an array of objects indicating images used in the post (object, optional)
+  * `file`: the name of the image file itself as archived from https://qposts.online (string)
+  * `name`: the name of the image as it was named when posted to the image board (string)
+* `referenced_posts`: an array of objects indicating replied-to posts within Q's post (i.e. `>>8251669`) (object, optional)
+  * `reference`: the string within the `text` of the main post that referred to this one (string)
+  * `text`: the textual content of the referenced post (string, optional)
+  * `images`: an array of objects indicating images used in the post (object, optional)
+    * `file`: the name of the image file itself as archived from https://qposts.online (string)
+    * `name`: the name of the image as it was named when posted to the image board (string)
+
+
+## Fine Print
+
+I provide this data for data analysis use only; the content is distasteful and misleading to put it charitably and I do not endorse it.
+
+The site itself is laid out mostly logically in terms of HTML and formatting so I have high hopes for consistency over time as it pertains to the screen scraping, but should it change dramatically, this extraction script will obviously break. I've tried to lay the script out as modularly as I can so that updates can be made with a reasonable amount of effort but I make no guarantees of durability, nor that I will have time or interest to update the script to stay current, to be brutally honest.
+
+The code in my extraction script is licensed under MIT (and please cite me if my script or its results is utilized as part of academic research -- I'd love to read a preprint!); as the extracted posts are not my content, I cannot license them in any degree.
---
+++ collate.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python3
+
+import copy
+import json
+import os
+
+from bs4 import BeautifulSoup
+
+DIRECTORY = 'qposts.online/page'
+
+
+def extract_metadata_block(meta_container):
+    collated_metadata = {}
+
+    # extract the spane with the name+tripcode in it
+    author_container = meta_container.find('span', 'name')
+    author = author_container.find('strong').getText()
+    if not len(author):
+        print('NAME NOT FOUND!', file=sys.stderr)
+        exit()
+    collated_metadata['author'] = author
+
+    # remove the base name, leaving the tripcode if applicable
+    author_container.find('strong').decompose()
+    tripcode_or_empty = author_container.getText().strip()
+    if tripcode_or_empty:
+        collated_metadata['tripcode'] = tripcode_or_empty
+
+    # extract source + board
+    source_container = meta_container.find('span', 'source')
+    board = source_container.find('strong').getText()
+    if not len(board):
+        print('BOARD NOT FOUND!', file=sys.stderr)
+        exit()
+    collated_metadata['source'] = {}
+    collated_metadata['source']['board'] = board
+
+    # remove the board name, leaving the site (and maybe link)
+    source_container.find('strong').decompose()
+
+    # get thread link if we have it
+    thread_link_or_empty = source_container.find('a')
+    if thread_link_or_empty:
+        collated_metadata['source']['link'] = thread_link_or_empty['href']
+        thread_link_or_empty.decompose()
+
+    site = source_container.getText().strip()
+    if site:
+        collated_metadata['source']['site'] = site
+    else:
+        print('SITE NOT FOUND!', file=sys.stderr)
+        exit()
+
+    # attach timestamp
+    collated_metadata['time'] = int(meta_container.find('span', 'time').getText())
+
+    return collated_metadata
+
+
+def extract_images(post_block):
+    images_container = post_block.find('div', 'images', recursive=False)
+
+    if not images_container:
+        return None
+
+    images = images_container.findAll('figure', recursive=False)
+    return [{
+        'file': os.path.split(image.find('a')['href'])[1],
+        'name': image.find('figcaption').getText()
+    } for image in images]
+
+
+def extract_body(post_block, is_ref=False):
+    # do decomposition with a local copy -- not sure if this is necessary
+    post_block_copy = copy.copy(post_block)
+
+    try:
+        # just attempt to find it; some main posts have it, some don't, no references have it
+        content_div = post_block_copy.find('div', 'text')
+        if content_div:
+            post_block_copy = content_div
+    except AttributeError:
+        pass
+    divs = post_block_copy.findAll('div')
+    for div in divs:
+        div.decompose()
+
+    return post_block_copy.get_text(separator="\n")
+
+
+def extract_references(post_block):
+    refs = post_block.findAll('div', 'op')
+    if not refs:
+        return None
+
+    collated_refs = []
+    for ref in refs:
+        collated_ref = {}
+        collated_ref['reference'] = ref.previous_sibling.getText()
+        maybe_text = extract_body(ref, is_ref=True)
+        if maybe_text:
+            collated_ref['text'] = maybe_text
+
+        maybe_images = extract_images(ref)
+        if maybe_images:
+            collated_ref['images'] = maybe_images
+
+        collated_refs.append(collated_ref)
+
+    return collated_refs
+
+
+# this is so dumb but the author uses a server-side email protection
+# script, I guess for anti-spam, but it's a little overzealous. Thankfully, usage is
+# minimal so I just wrote a function to slot them in from the known list
+def clean_up_emails(post):
+    if post['post_metadata']['time'] == 1526767434:
+        post['post_metadata']['author'] = 'NowC@mesTHEP@in—-23!!!'
+
+    if post['post_metadata']['time'] in [1588693786, 1585242439, 1553795409]:
+        post['text'] = post['text'].replace('email\xa0protected]',
+                                            'https://uscode.house.gov/view.xhtml?path=/prelim@title'
+                                            '18/part1/chapter115&edition=prelim')
+
+    return post
+
+
+collected_posts = []
+for entry in os.scandir(DIRECTORY):
+    # helpful for debugging
+    # if entry.name != '64.html':
+    #     continue
+
+    soup = BeautifulSoup(open(entry.path), 'html.parser')
+
+    # extract all posts
+    posts = soup.findAll('div', {'class': 'post', 'data-timestamp': True})
+
+    for post in posts:
+        collated_post = {}
+        # helpful for debugging
+        # collated_post['scrape_metadata'] = {
+        #     'file': entry.name,
+        #     'id': int(post.find('div', 'meta').find('span', 'num').getText())
+        # }
+
+        # yank metadata
+        meta_container = post.find('div', 'meta')
+        collated_post['post_metadata'] = extract_metadata_block(meta_container)
+
+        # break out main meat of the post for easier manipulation
+        post_body = post.find('div', 'message')
+
+        # yank images
+        extracted_images = extract_images(post_body)
+        if extracted_images:
+            collated_post['images'] = extracted_images
+
+        # yank main post text
+        extracted_body = extract_body(post_body)
+        if extracted_body:
+            collated_post['text'] = extracted_body
+
+        # yank referenced posts
+        referenced_posts = extract_references(post_body)
+        if referenced_posts:
+            collated_post['referenced_posts'] = referenced_posts
+
+        # clean up emails -- see func comment
+        collated_post = clean_up_emails(collated_post)
+
+        collected_posts.append(collated_post)
+
+
+# just a little pretty printing :)
+with open('posts.json', 'w') as outfile:
+    json.dump(collected_posts, outfile, indent=2, ensure_ascii=False)
